17b91e22dab952bd297c7d0a1adb3d5142eb816b
KimTan021 <tankimemerson05@gmail.com>
2024-07-06 23:28:24 +0800

    modify app.js and vercel.json

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require("dotenv").config();
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
